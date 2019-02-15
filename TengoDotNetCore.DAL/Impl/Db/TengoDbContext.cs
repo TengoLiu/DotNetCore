a0edd3418a75980fbf4309ec45121d92b388fa90
tengo_liu <liu5087120!@163.com>
2019-02-15 12:59:34 +0800

    移除无用文件夹

--- TengoDotNetCore.DAL/Impl/Db/TengoDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.EntityFrameworkCore;
-
-namespace TengoDotNetCore.DAL.Impl.Db {
- public   class TengoDbContext :DbContext{
-    }
-}
--- TengoDotNetCore.Model/ViewModel/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace TengoDotNetCore.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
