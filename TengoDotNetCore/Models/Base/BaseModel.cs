6be013584728c5bd7dfb81e229c564490b4c7639
tengo_liu <liu5087120!@163.com>
2019-02-18 15:43:13 +0800

    添加对基础Model的注释

--- TengoDotNetCore/Models/Base/BaseModel.cs
+++ TengoDotNetCore/Models/Base/BaseModel.cs
@@ -5,10 +5,17 @@ using System.Threading.Tasks;
 
 namespace TengoDotNetCore.Models.Base {
 
+    /// <summary>
+    /// 基础的Model，包含一些公用的属性，比如添加时间和修改时间
+    /// </summary>
     public class BaseModel {
-
+        /// <summary>
+        /// 添加时间
+        /// </summary>
         public DateTime AddTime { get; set; }
-
+        /// <summary>
+        /// 修改时间
+        /// </summary>
         public DateTime UpdateTime { get; set; }
     }
 }
