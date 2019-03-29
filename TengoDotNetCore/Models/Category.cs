eebe98613c3d79fdc85233cc7c5442191d35e217
tengo_liu <liu5087120!@163.com>
2019-03-29 09:51:48 +0800

    将树状结点的Model提取出基类

--- TengoDotNetCore/Models/Category.cs
+++ TengoDotNetCore/Models/Category.cs
@@ -6,33 +6,10 @@ using System.Threading.Tasks;
 using TengoDotNetCore.Models.Base;
 
 namespace TengoDotNetCore.Models {
-    public class Category : BaseModel {
-        /// <summary>
-        /// 父结点的ID
-        /// </summary>
-        public int ParID { get; set; }
-
-        /// <summary>
-        /// 分类的级别
-        /// 这个字段，可以说是冗余的，但是却可以辅助一下来获取某一个级别的数据
-        /// </summary>
-        public int Level { get; set; }
-
-        /// <summary>
-        /// 分类名称
-        /// </summary>
-        [Required(ErrorMessage = "名称是必填的")]
-        public string Name { get; set; }
-
-        /// <summary>
-        /// 排序，排序越大越靠前
-        /// </summary>
-        public int Sort { get; set; }
-
-        /// <summary>
-        /// 封面图片
-        /// </summary>
-        public string CoverImg { get; set; }
+    /// <summary>
+    /// 商品的分类，树形结构
+    /// </summary>
+    public class Category : BageTreeNode {
 
         /// <summary>
         /// 分类对应的商品
