828f897638eced5815e8c12c983ae6a45e8d53e7
tengo_liu <liu5087120!@163.com>
2019-07-30 11:28:21 +0800

    添加了在main中调用DI容器的里面服务的方法

--- TengoDotNetCore/Program.cs
+++ TengoDotNetCore/Program.cs
@@ -1,10 +1,29 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace TengoDotNetCore {
     public class Program {
         public static void Main(string[] args) {
             var host = CreateWebHostBuilder(args).Build();
+
+            #region 如果需要在Main启动方法的时候获取注册好的服务的话，可以这么写
+            //using (var serviceScope = host.Services.CreateScope()) {
+            //    var services = serviceScope.ServiceProvider;
+
+            //    try {
+            //       //这里就可以获取注册的服务
+            //        var serviceContext = services.GetRequiredService<MyService>();
+            //        // Use the context here
+            //    }
+            //    catch (Exception ex) {
+            //        var logger = services.GetRequiredService<ILogger<Program>>();
+            //        logger.LogError(ex, "An error occurred.");
+            //    }
+            //}
+            #endregion
+
             host.Run();
         }
 
