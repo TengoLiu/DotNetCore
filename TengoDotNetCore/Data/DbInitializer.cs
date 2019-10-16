83842dba8040d3f663eefa5d173990d8d91d9338
tengo_liu <liu5087120!@163.com>
2019-10-16 16:35:17 +0800

    对数据库初始化的类添加了一些无关紧要的注释。

--- TengoDotNetCore/Data/DbInitializer.cs
+++ TengoDotNetCore/Data/DbInitializer.cs
@@ -2,7 +2,13 @@
 using TengoDotNetCore.Models;
 
 namespace TengoDotNetCore.BLL.Data {
+    /// <summary>
+    /// 数据库初始化类
+    /// 说明：网站启动的时候对数据库的初始化操作。比如检查某些表是否为空，如果为空的话需要插入一些基础数据。
+    /// 时间：2019年10月16日
+    /// </summary>
     public static class DbInitializer {
+     
         public static void Initialize(TengoDbContext db) {
 
             // 如果文章分类表为空的话，那么初始化数据
